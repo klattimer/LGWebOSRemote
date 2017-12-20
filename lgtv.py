741b941b2aaec90a6ac96562724c3628905da9b5
Karl Lattimer <karl@qdh.org.uk>
2017-12-20 14:40:08 +0000

    Update lgtv.py
    oops

--- lgtv.py
+++ lgtv.py
@@ -1,6 +1,6 @@
+from __future__ import print_function
 import sys
 import json
-from __future__ import print_function
 from inspect import getargspec
 from LGTV import LGTVScan, LGTVClient, getCommands
 
