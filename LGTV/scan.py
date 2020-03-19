bddd4a570eca7d867a793699abaffc74df2a85fa
129fc5e32c631b5007cb2cd8d45d416002b42bab <16278483+DarkCeptor44@users.noreply.github.com>
2020-03-18 21:03:40 -0300

    Scan Semi-Fix

--- LGTV/scan.py
+++ LGTV/scan.py
@@ -11,38 +11,37 @@ def LGTVScan():
               b'MX: 2\r\n' \
               b'ST: urn:schemas-upnp-org:device:MediaRenderer:1\r\n\r\n'
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(1)
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(1)
 
-    addresses = []
-    attempts = 4
-    for i in range(attempts):
-        sock.sendto(request, (b'239.255.255.250', 1900))
-        uuid = None
-        model = None
-        address = None
-        data = {}
-        response, address = sock.recvfrom(512)
-        for line in response.split(b'\n'):
-            if line.startswith(b'USN'):
-                uuid = re.findall(r'uuid:(.*?):', line.decode('utf-8'))[0]
-            if line.startswith(b'DLNADeviceName'):
-                (junk, data) = line.split(b':')
-                data = data.strip().decode('utf-8')
-                model = re.findall(r'\[LG\] webOS TV (.*)', unquote(data))[0]
-            data = {
-                'uuid': uuid,
-                'model': model,
-                'address': address[0]
-            }
+        addresses = []
+        attempts = 4
+        for i in range(attempts):
+            sock.sendto(request, (b"239.255.255.250", 1900))
+            uuid = None
+            model = None
+            address = None
+            data = {}
+            response, address = sock.recvfrom(512)
+            for line in response.split(b"\n"):
+                if line.startswith(b"USN"):
+                    uuid = re.findall(r"uuid:(.*?):", line.decode("utf-8"))[0]
+                if line.startswith(b"DLNADeviceName"):
+                    (junk, data) = line.split(b":")
+                    data = data.strip().decode("utf-8")
+                    model = re.findall(r"\[LG\] webOS TV (.*)", unquote(data))[0]
+                data = {"uuid": uuid, "model": model, "address": address[0]}
 
-        if re.search(b'LG', response):
-            addresses.append(data)
-        else:
-            print ('Unknown device')
-            print (response, address)
-        sleep(2)
+            if re.search(b"LG", response):
+                addresses.append(data)
+            else:
+                print("Unknown device")
+                print(response, address)
+            sleep(2)
 
-    sock.close()
-    addresses = list({x['address']: x for x in addresses}.values())
+        sock.close()
+        addresses = list({x["address"]: x for x in addresses}.values())
+    except socket.error as socketerror:
+        print("Error: ", socketerror)
     return addresses
