bee76efb9c42c24ed5d1258ef108f2a53bc640c4
Karl Lattimer <karl@qdh.org.uk>
2023-12-04 07:57:16 +0000

    Update cursor.py with command codes.

--- LGTV/cursor.py
+++ LGTV/cursor.py
@@ -13,5 +13,26 @@ class LGTVCursor(WebSocketClient):
     def __init__(self, name, ip=None, mac=None, key=None, hostname=None):
         self.remote = LGTVRemote(name, ip, mac, key, hostname)
         self.remote.connect()
+        self.remote.getCursorSocket(self.__finalize)
 
-        self.remote.execute("getCursorSocket", callback=self.__finalize)
+    def up(self):
+        self.send("type:button\nname:UP\n\n")
+        
+    def down(self):
+        self.send("type:button\nname:DOWN\n\n")
+
+    def left(self):
+        self.send("type:button\nname:LEFT\n\n")
+
+    def right(self):
+        self.send("type:button\nname:RIGHT\n\n")
+
+    def click(self):
+        self.send("type:click\n\n\n")
+
+    def back(self):
+        self.send("type:button\nname:BACK\n\n")
+
+    def enter(self)
+        self.remote.sendEnterKey()
+    
